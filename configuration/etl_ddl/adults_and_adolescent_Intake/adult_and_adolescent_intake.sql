daccda2c11b9ece74459ed542bd65a45a85f819a
Michael Ndula <michaelndula@gmail.com>
2023-11-10 09:29:33 +0300

    adding patient_id and encounter_id

--- configuration/etl_ddl/adults_and_adolescent_Intake/adult_and_adolescent_intake.sql
+++ configuration/etl_ddl/adults_and_adolescent_Intake/adult_and_adolescent_intake.sql
@@ -6,6 +6,8 @@ DROP TABLE IF EXISTS flat_encounter_adult_and_adolescent_intake;
 
 CREATE TABLE flat_encounter_adult_and_adolescent_intake
 (
+    patient_id                                        INT,
+    encounter_id                                      INT,
     date_of_enrollment                                DATE,
     marital_status                                    VARCHAR(255),
     past_medical_history                              VARCHAR(255),
